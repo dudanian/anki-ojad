8193b4f6533a0917959c116cdfe88b370aa31b7a
Andrew Duda <dudanian@gmail.com>
2019-10-12 17:55:17 +0900

    anki-pronunciation: add support for hiragana
    Add the ability to convert katakana to hiragana for hiragana words
    because I don't like reading pronunciations in katakana for everything.

--- nhk/dict.py
+++ nhk/dict.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import re
 
 from collections import namedtuple
 
@@ -7,11 +8,9 @@ PITCH_RAISE = "["
 PITCH_LOWER = "]"
 PITCH_STEADY = "-"
 
-#DIR_PATH = os.path.dirname(os.path.normpath(__file__))
-DIR_PATH = "/home/aduda/.local/share/Anki2/addons21/something/else"
+DIR_PATH = os.path.dirname(os.path.normpath(__file__))
 NHK_PATH = os.path.normpath(os.path.join(DIR_PATH, "..", "..", "932119536"))
-RAW_NHK_DICT = os.path.join(NHK_PATH, "ACCDB_unicode.csv")
-NHK_DICT = os.path.join(DIR_PATH, "dict.txt")
+NHK_DICT = os.path.join(NHK_PATH, "ACCDB_unicode.csv")
 
 nhk_dict = {}
 
@@ -34,14 +33,55 @@ AccentEntry = namedtuple('AccentEntry', [
     'midashigo1', # pronunciation display (in katakana)
     'akusentosuu', # number of accents (relates other entries)
     'bunshou', # ???
-    'ac', # accent length, if shorter than numchars, have to 0 pad front
+    'ac', # accent, if shorter than numchars, have to 0 pad front
 ])
 
+def is_katakana(char):
+    return bool(re.search("[ァ-ヴヷ-ヺ]", char))
+
+KATAKANA_TO_HIRAGANA = ord("ぁ") - ord("ァ")
+def to_hiragana(value):
+    if value >= "ァ" and value <= "ヴ":
+        return chr(ord(value) + KATAKANA_TO_HIRAGANA)
+    else:
+        return value
+
+def correct_hiragana_mixed(key, value):
+    i = 0
+    # append garbage to key so it has the same length
+    key = key + "_"*(len(value)-len(key))
+    corrected = ""
+
+    for k, v in zip(key, value):
+        # cases when key actually includes katakana
+        if k == v:
+            corrected += v
+        # else this is most likely hiragana to convert
+        elif is_katakana(v):
+            corrected += to_hiragana(v)
+        # else probably a symbol we don't want to do anything
+        else:
+            corrected += v
+
+    return corrected
+
+def correct_hiragana(value):
+    return "".join(map(to_hiragana, value))
+
+def correct_word(key, value):
+    if is_katakana(key[0]):
+        return correct_hiragana_mixed(key, value)
+    elif is_katakana(key[-1]):
+        return correct_hiragana_mixed(key[::-1], value[::-1])[::-1]
+    else:
+        return correct_hiragana(value)
+
 def format_accent(entry):
-    chars = entry.midashigo1
     # zero pad the accent
     accents = entry.ac
     accents = "0"*(len(chars)-len(accents))+accents
+    # correct the kana
+    chars = correct_word(entry.nhk, entry.midashigo1)
 
     word = ""
     last = "0"
@@ -59,7 +99,7 @@ def format_accent(entry):
     return word
 
 def build_dict():
-    for entry in map(AccentEntry._make, csv.reader(open(RAW_NHK_DICT, "r"))):
+    for entry in map(AccentEntry._make, csv.reader(open(NHK_DICT, "r"))):
         value = format_accent(entry)
         for key in [entry.nhk, entry.kanjiexpr]:
             if key in nhk_dict:
@@ -74,6 +114,3 @@ def get_pronunciations(text):
 
     return nhk_dict.get(text, [])
 
-
-if __name__ == "__main__":
-    build_dict()
